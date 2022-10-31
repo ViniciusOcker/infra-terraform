f114d0ff3a3a0a39226ed7d34d885bdc01cfdb2b
Vinicius Ocker Fagundes <60163731+ViniciusOcker@users.noreply.github.com>
2022-10-31 20:50:11 -0300

    Aula ansible

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "server" {
+resource "aws_instance" "ec2-server" {
   count         = 2
   ami           = var.ami
   instance_type = var.tipo_instancia
@@ -7,10 +7,23 @@ resource "aws_instance" "server" {
     "Name" = "${var.usuario}-ec2-${count.index}"
   }
   vpc_security_group_ids = [aws_security_group.sg_acesso_ssh_publico.id, aws_security_group.sg_acesso_web.id]
+}
+
+resource "aws_instance" "ec2-ansible" {
+  count         = 1
+  ami           = var.ami
+  instance_type = var.tipo_instancia
+  key_name      = "${var.usuario}-terraform-aws"
+  tags = {
+    "Name" = "${var.usuario}-ec2-ansible-${count.index}"
+  }
+  vpc_security_group_ids = [aws_security_group.sg_acesso_ssh_publico.id, aws_security_group.sg_acesso_web.id]
 
   user_data = <<-EOF
-    #!/bin/bash
-    sudo apt update
-    sudo apt install nginx -y
+  #!/bin/bash
+  sudo apt update
+  sudo apt install software-properties-common -y
+  sudo add-apt-repository --yes --update ppa:ansible/ansible -y
+  sudo apt-get install ansible -y
   EOF
 }
--- output.tf
+++ output.tf
@@ -1,7 +1,11 @@
-output "instancias_a2" {
-  value = aws_instance.server.*.public_ip
+output "instancias-ec2" {
+  value = aws_instance.ec2-server.*.public_ip
 }
 
-output "instancias_a2_private" {
-  value = aws_instance.server.*.private_ip
+output "instancias-ec2-ansible" {
+  value = aws_instance.ec2-ansible.*.public_ip
+}
+
+output "instancias-ec2-private" {
+  value = aws_instance.ec2-server.*.private_ip
 }
